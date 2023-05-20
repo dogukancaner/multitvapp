7d4e97bb7f4790fffead198cf848e98675689a81
dogukancaner <dogukanncanerr@gmail.com>
2023-05-20 23:53:28 +0300

    Added data folder and news data

---
+++ data/news/News.js
@@ -0,0 +1,14 @@
+export const NewsData = [
+  {
+    title: "CNN TÜRK",
+    URL: "https://www.youtube.com/live/X_EWYemclKA?feature=share",
+  },
+  {
+    title: "HABERTÜRK",
+    URL: "https://www.youtube.com/live/SqHIO2zhxbA?feature=share",
+  },
+  {
+    title: "TRT HABER",
+    URL: "https://www.youtube.com/live/-Lrxv1_i3qc?feature=share",
+  },
+];
